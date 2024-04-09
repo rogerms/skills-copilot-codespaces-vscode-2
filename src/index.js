3bb23fe732f577a14349f96ae126930c543be5c1
rogerms <sirsroger@gmail.com>
2024-04-09 02:55:25 +0000

    refactor src folder

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.send('<div style="margin: 10px; padding: 10px 20px; color:red; font-weight:bold" >Hello, World!</div>');
 });
 
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/about.html');
+});
+
 
 
 app.listen(port, () => {
